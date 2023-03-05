5cb15b776fa248bcd750f3370312d204eeeaedb1
Keks <keks@htmlacademy.ru>
2023-03-05 19:12:12 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,22 +1,10 @@
-// id, число — идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
-
-// url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
-
-// description, строка — описание фотографии. Описание придумайте самостоятельно.
-
-// likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-
-// comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии.
-// Количество комментариев к каждой фотографии вы определяете на своё усмотрение.
-// Все комментарии генерируются случайным образом. Пример описания объекта с комментарием:
-
 const PICTURE_COUNT = 25;
-// eslint-disable-next-line no-unused-vars
 const AVATAR_COUNT = 6;
 const LIKE_MIN_COUNT = 15;
 const LIKE_MAX_COUNT = 200;
-const COMMENT_COUNT = 20;
-const COMMENT_LINES = [
+const MAX_COMMENT_INDEX = 1000;
+const MAX_COMMENT_COUNT = 5;
+const COMMENT_MESSEGE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -26,10 +14,12 @@ const COMMENT_LINES = [
 ];
 const DESCRIPTIONS = [
   'Описание фото1', 'Описание фото2', 'Описание фото3', 'Описание фото4', 'Описание фото5', 'Описание фото6', 'Описание фото7',
-  'Описание фото8', 'Описание фото9', 'Описание фото10',
+  'Описание фото8', 'Описание фото9', 'Описание фото10', 'Описание фото11', 'Описание фото12', 'Описание фото13', 'Описание фото14',
+  'Описание фото15', 'Описание фото16', 'Описание фото17', 'Описание фото18', 'Описание фото19', 'Описание фото20'
 ];
 
-const NAMES = ['Иван',
+const NAMES = [
+  'Иван',
   'Хуан Себастьян',
   'Мария',
   'Кристоф',
@@ -46,7 +36,8 @@ const NAMES = ['Иван',
   'Иван Степанович',
   'Волан-де-Морт',
   'Азазелло',
-  'Пендальф Серый'];
+  'Пендальф Серый'
+];
 
 //генерация рамдомного ID элемента
 const getRandomInteger = (a, b) => {
@@ -56,49 +47,34 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-//генерация рамдомного элемента из массива
-const getRandomArrayElement = (array) =>
-  array[getRandomInteger(0, array.length - 1)];
+// функция по поиску случайного элемента в массиве
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
 
-const createIdGenerator = () => {
-  let lastGeneratedId = 0;
+// функция создает массив комментариев
+const createComment = () => {
+  const randomAvatarIndex = getRandomInteger(1, AVATAR_COUNT); //создает аватар
 
-  return () => {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
+  return {
+    id: getRandomInteger(1, MAX_COMMENT_INDEX),
+    avatar: `img/avatar-${randomAvatarIndex}.svg`,
+    message: getRandomArrayElement(COMMENT_MESSEGE), // случайное сообщение из массива MESSEGE
+    name: getRandomArrayElement(NAMES), // случайное Имя из массива NAMES
   };
 };
 
-//генаратор комментариев
-const generateCommentId = createIdGenerator();
+// функция создает объекты
+const createPicture = (pictureId) => {
+  const randomLikeIndex = getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT); // рамдомное колличество лайков
 
-const createMessage = () =>
-  Array.from({ length: getRandomInteger(1, 2) }, () =>
-    getRandomArrayElement(COMMENT_LINES)).join(' ');
+  return {
+    id: pictureId,
+    url: `photos/${pictureId}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS), // случайное описание фото из массива DESCRIPTIONS
+    like: randomLikeIndex, // колличество лайков
+    comments: Array.from({ length: getRandomInteger(1, MAX_COMMENT_COUNT) }, createComment),
 
-const createComment = () => ({
-  id: generateCommentId(),
-  avatar: 'img/avatar-$ {getRandomInteger(1, AVATAR_COUNT)}.svg',
-  message: createMessage(),
-  name: getRandomArrayElement(NAMES),
-});
+  };
+};
 
-
-const createPicture = (index) => ({
-  id: index,
-  url: 'photos/$ {index}.jpg',
-  description: getRandomArrayElement(DESCRIPTIONS),
-  lekes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
-  comments: Array.from(
-    { length: getRandomInteger(0, COMMENT_COUNT) },
-    createComment
-  ),
-});
-
-const getPictures = () =>
-  Array.from({ length: PICTURE_COUNT }, (_, pictureIndex) =>
-    createPicture(pictureIndex + 1)
-  );
-
-getPictures();
-
+const similarPicture = new Array(PICTURE_COUNT).fill().map((_, index) => createPicture(index + 1));
+similarPicture();
